3243fcf5055f276acc5a31a5bcbcd8a1bb4b8192
Zhehai <zhehaizhang1@gmail.com>
2022-08-27 04:04:42 -0400

    Detect and sort garbage

--- internal/sorter/api.go
+++ internal/sorter/api.go
@@ -8,21 +8,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-type ImageContent struct {
-	Content string
-}
-
-type ImageType struct {
-	Image ImageContent
-}
-
-type GoogleRequest struct {
-	Requests []ImageType
-}
-
 func ClassifyHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println(vars["userId"])
 	var requestBody entity.Classify
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
@@ -30,9 +23,23 @@ func ClassifyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	imagePayload := GoogleRequest{
-		Requests: []ImageType{
-			ImageType{Image: ImageContent{Content: requestBody.Contents}},
+	fmt.Println("Done decoding request body")
+
+	imagePayload := entity.GoogleRequest{
+		Requests: []entity.ImageType{
+			entity.ImageType{
+				Image: entity.ImageContent{Content: requestBody.Contents},
+				Features: []entity.FeatureContent{
+					entity.FeatureContent{
+						Type:       "LABEL_DETECTION",
+						MaxResults: 15,
+					},
+					entity.FeatureContent{
+						Type:       "OBJECT_LOCALIZATION",
+						MaxResults: 15,
+					},
+				},
+			},
 		},
 	}
 
@@ -40,17 +47,73 @@ func ClassifyHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		fmt.Println("json error")
+		return
+	}
+	fmt.Println("Done making request body")
+
+	url := fmt.Sprintf("https://vision.googleapis.com/v1/images:annotate?key=%s", os.Getenv("API_KEY"))
+
+	//fmt.Println(url)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	resp, err := http.Post(fmt.Sprintf("https://vision.googleapis.com/v1/images:annotate/%s", os.Getenv("API_KEY")), "application/json", bytes.NewBuffer(payload))
+	defer resp.Body.Close()
+
+	var m entity.GoogleResponse
 
 	body, err := ioutil.ReadAll(resp.Body)
 
-	if err != nil {
-		fmt.Println("error parsing body")
+	if err = json.Unmarshal(body, &m); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	sb := string(body)
+	// sb := string(body)
 
-	fmt.Println(sb)
+	var response entity.ClassifyResponse
+
+	blueBinPossible, err := getBlueBin()
+	redBinPossible, err := getRedBin()
+
+	found := "Garbage"
+
+	for _, s := range m.Responses[0].LocalizedObjectAnnotations {
+		name := strings.ToLower(s.Name)
+		blueExists := blueBinPossible[name]
+		if blueExists {
+			found = "blue"
+			break
+		}
+		redExists := redBinPossible[name]
+		if redExists {
+			found = "red"
+			break
+		}
+	}
+
+	for _, s := range m.Responses[0].LabelAnnotations {
+		name := strings.ToLower(s.Description)
+		blueExists := blueBinPossible[name]
+		if blueExists {
+			found = "blue"
+			break
+		}
+		redExists := redBinPossible[name]
+		if redExists {
+			found = "red"
+			break
+		}
+	}
+	response.Type = found
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		return
+	}
+
+	w.Write(jsonResponse)
 }
