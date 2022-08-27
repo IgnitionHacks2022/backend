d0d23211d085ff9e6c92449645ea3628ac9ce452
aaggarwal10 <anish.5@hotmail.com>
2022-08-27 01:30:36 -0400

    Breaking Auth on Error

--- internal/auth/api.go
+++ internal/auth/api.go
@@ -19,21 +19,25 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&lR)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	conn, err := db.Connection()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	userID, err := db.UserCheckCreds(conn, lR.Email, lR.Password)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
 	}
 
 	token, err := GenerateToken(userID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	json.NewEncoder(w).Encode(LoginResponse{Token: token})
 }
@@ -44,39 +48,45 @@ type RegisterResponse struct {
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	var rR entity.RegisterRequest
-
 	err := json.NewDecoder(r.Body).Decode(&rR)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	conn, err := db.Connection()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	registered, err := db.UserCheckRegistered(conn, rR.Email)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	if registered {
 		http.Error(w, "Username or email already taken", http.StatusConflict)
+		return
 	}
 
 	hashed, err := bcrypt.GenerateFromPassword([]byte(rR.Password), bcrypt.MinCost)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	newUser := db.User{Email: rR.Email, BluetoothID: rR.BluetoothID, Password: string(hashed)}
 	userID, err := db.UserRegister(conn, &newUser)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	token, err := GenerateToken(userID)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	json.NewEncoder(w).Encode(RegisterResponse{Token: token})
 }
